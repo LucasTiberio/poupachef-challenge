a7880f86d47c7f9b5d0a8932b2b11df9fb34e4c3
Lucas Tiberio <ltiberio55@gmail.com>
2021-02-21 17:46:03 -0300

    fix/Logos inside project

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
 
+import CompactLogo from '@poupachef/assets/images/compact_logo.png';
+import Logo from '@poupachef/assets/images/logo.png';
+
 import ThemeSwitcher from '../ThemeSwitcher';
 import { Container, IconImage, LogoImage } from './style';
 
@@ -7,8 +10,8 @@ const Header = (): JSX.Element => {
   return (
     <Container>
       <div>
-        <IconImage src="https://play-lh.googleusercontent.com/sk4tzy1hVZVTzTNx7exV01HOae3Nuiuy8y6J7556l7cFWJZ6NiFvXOh1EQFxUW_vviI" />
-        <LogoImage src="https://uploads-ssl.webflow.com/5e0517740aa529fa437bc1a2/5fbfa1730ef9e6ea14d23288_Group%203.svg" />
+        <IconImage src={CompactLogo} />
+        <LogoImage src={Logo} />
       </div>
       <ThemeSwitcher />
     </Container>
